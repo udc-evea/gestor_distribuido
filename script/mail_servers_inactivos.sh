2fdeb087003fe6d35d1d2ffb1a3c78421cd19506
mppfiles <mppfiles@gmail.com>
2014-02-23 10:35:44 -0300

    actualizo definicion para proceso de mail inactivos (falta testear)

--- script/mail_servers_inactivos.sh
+++ script/mail_servers_inactivos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # OJO...el host si o si debe ser "localhost"
-wget http://localhost/udc_apps/backend.php/servers/reporteInactivos
+wget http://localhost/gestor_distribuido/index.php/server/reporteInactivos
